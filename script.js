1f8c8dc59f70edad998266048ae87709c4b1ba68
JeffJohn22 <jefri01.j@zifornd.com>
2024-05-28 13:01:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -115,41 +115,25 @@ window.onload = function () {
 
    frame1 = function() {
      draw (0, 150, 150, 150);
-   };
-   
-   frame2 = function() {
      draw (10, 0, 10, 600);
-   };
-  
-   frame3 = function() {
      draw (0, 5, 70, 5);
-   };
-  
-   frame4 = function() {
      draw (60, 5, 60, 15);
    };
-  
    torso = function() {
      draw (60, 36, 60, 70);
    };
   
    rightArm = function() {
      draw (60, 46, 100, 50);
-   };
-  
-   leftArm = function() {
      draw (60, 46, 20, 50);
    };
   
-   rightLeg = function() {
+  rightLeg = function() {
      draw (60, 70, 100, 100);
-   };
-  
-   leftLeg = function() {
      draw (60, 70, 20, 100);
    };
-  
-  drawArray = [rightLeg, leftLeg, rightArm, leftArm,  torso,  head, frame4, frame3, frame2, frame1]; 
+    
+  drawArray = [rightLeg, rightArm, torso, frame1]; 
 
 
   // OnClick Function
@@ -189,7 +173,7 @@ window.onload = function () {
     buttons();
 
     geusses = [ ];
-    lives = 10;
+    lives = 4;
     counter = 0;
     space = 0;
     result();
